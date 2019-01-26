c11d2152b2080f61c711ca65b826c5c2d759670e
Alex Hunt <alex.s.hunt7@gmail.com>
2019-01-25 20:59:44 -0800

    godoc

--- ssher.go
+++ ssher.go
@@ -1,3 +1,29 @@
+/*
+Package ssher allows you to easily create ssh connections in golang, using values from your ssh config file.
+
+Most people will already have configured their ssh connections once for openssh, so why do it again manually?
+
+This package is essentially a wrapper around https://github.com/kevinburke/ssh_config,
+which attempts to automatically load all required values from your ~/.ssh/config.
+
+
+Currently supported options:
+* Ciphers
+* ConnectTimeout
+* HostKeyAlgorithms
+* Hostname
+* IdentityFile
+* KexAlgorithms
+* MACs
+* Port
+* User
+* UserKnownHostsFile
+
+
+Currently, only public key auth is attempted and only the agent and one IdentityFile will be used.
+Host key checking is enforced.
+Any limitations that apply to https://github.com/kevinburke/ssh_config also apply here (currently no support for +value syntax).
+*/
 package ssher
 
 import (
@@ -16,6 +42,7 @@ import (
 	"time"
 )
 
+// PublicKeyFile takes a path to an IdentityFile, reads it, and parses it into an ssh.AuthMethod.
 func PublicKeyFile(file string) ssh.AuthMethod {
 	buffer, err := ioutil.ReadFile(file)
 	if err != nil {
@@ -29,7 +56,7 @@ func PublicKeyFile(file string) ssh.AuthMethod {
 	return ssh.PublicKeys(key)
 }
 
-type SSHConfig interface {
+type sshConfig interface {
 	Get(string, string) string
 }
 
@@ -45,6 +72,8 @@ func (cw *configWrapper) Get(alias, key string) string {
 	return val
 }
 
+// SSHAgent connects to the ssh agent defined by the SSH_AUTH_SOCK environment variable,
+// and returns an ssh.PublicKeysCallback ssh.AuthMethod if successful.
 func SSHAgent() ssh.AuthMethod {
 	if sshAgent, err := net.Dial("unix", os.Getenv("SSH_AUTH_SOCK")); err == nil {
 		return ssh.PublicKeysCallback(agent.NewClient(sshAgent).Signers)
@@ -52,8 +81,8 @@ func SSHAgent() ssh.AuthMethod {
 	return nil
 }
 
-func decodeSSHConfig(configFile string) (SSHConfig, error) {
-	var userConfig SSHConfig
+func decodeSSHConfig(configFile string) (sshConfig, error) {
+	var userConfig sshConfig
 	var err error
 
 	if configFile == "" {
@@ -76,6 +105,10 @@ func decodeSSHConfig(configFile string) (SSHConfig, error) {
 	return userConfig, err
 }
 
+// ClientConfig takes in an ssh config file host alias and a path to an ssh config file,
+// and returns an ssh.ClientConfig and a connection string (for dialing).
+// If passed an empty string for the configFile, it will use the default config file paths:
+// ~/.ssh/config and /etc/ssh/ssh_config
 func ClientConfig(alias string, configFile string) (*ssh.ClientConfig, string, error) {
 	var err error
 	var connectHost string
